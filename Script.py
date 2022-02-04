82a794955e1d7ae7defbc44a31d2ea0a5c2ff3b4
kinh122 <97784528+kinh122@users.noreply.github.com>
2022-02-04 20:57:39 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,10 +1,10 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='http://t.me/PowerFilterRoBot'>AUTOFILTERBOT</a>, 🙋, 𝗜'𝗺  ⚡️⚡️ 𝗣𝗼𝘄𝗲𝗿𝗳𝘂𝗹 𝗔𝘂𝘁𝗼-𝗙𝗶𝗹𝘁𝗲𝗿 𝗕𝗼𝘁  ⚡️⚡️ 💪💪..🤓 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='http://t.me/IMDB10_BOT'>HITLER</a>, 🙋, 𝗜'𝗺  ⚡️⚡️ 𝗣𝗼𝘄𝗲𝗿𝗳𝘂𝗹 𝗔𝘂𝘁𝗼-𝗙𝗶𝗹𝘁𝗲𝗿 𝗕𝗼𝘁  ⚡️⚡️ 💪💪..🤓 😍"""
     HELP_TXT = """𝐇𝐄𝐘 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: 𝗔𝘂𝘁𝗼 𝗙𝗶𝗹𝘁𝗲𝗿 𝗕𝗼𝘁
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='http://t.me/PowerFilterRoBot'>AUTO FILTER BOT</𝐚>
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: HITLER
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='http://t.me/@Hari12357'>AUTO FILTER BOT</𝐚>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -15,7 +15,7 @@ class script(object):
 - Source - 𝗔𝘂𝘁𝗼 𝗙𝗶𝗹𝘁𝗲𝗿 𝗕𝗼𝘁 Is Not A Open Source
 
 <b>AutoFilterRoBot:</b>
-- <a href=http://t.me/PowerFilterRoBot>AUTO FILTER BOT</a>"""
+- <a href=http://t.me/@Hari12357>HITLER</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
